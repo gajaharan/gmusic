3cea4b9ba05950d42dd175fef9f270ed2e8be769
gajaharan <gajaharan@gmail.com>
2016-11-11 16:30:11 +0000

    Initial Commit

---
+++ gMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gMusic
+//
+//  Created by Gajaharan Satkunanandan on 11/11/2016.
+//  Copyright © 2016 Gajaharan Satkunanandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gMusic
+//
+//  Created by Gajaharan Satkunanandan on 11/11/2016.
+//  Copyright © 2016 Gajaharan Satkunanandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
